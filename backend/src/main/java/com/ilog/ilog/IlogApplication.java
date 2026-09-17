210024927782d6688162caf7d6ed8c0e0ec32b44
박창재 <xao300@gmail.com>
2026-09-17 17:30:53 +0900

    BE: 개발환경 구성

---
+++ backend/src/main/java/com/ilog/ilog/IlogApplication.java
@@ -0,0 +1,13 @@
+package com.ilog.ilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ilog/ilog/IlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilog.ilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
